4d02d13e872292145c2bea070902921668afbe7b
Prescott <carlsonprescott@gmail.com>
2020-01-27 17:20:02 -0800

    skeleton set up

--- src/cat_hopper.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const canvas = document.getElementById('catHopper');
-    const context = canvas.getContext('2d');
-
-});
---
+++ src/index.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const canvas = document.getElementById('catHopper');
+    const context = canvas.getContext('2d');
+});
